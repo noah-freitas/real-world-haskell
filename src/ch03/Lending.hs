f897cb2d9c906d79d66ce6d7f92031cf34e94329
Noah Freitas <noah.j.freitas@gmail.com>
2013-07-04 19:09:43 +0000

    ch03/Lending lend3

--- src/ch03/Lending.hs
+++ src/ch03/Lending.hs
@@ -12,3 +12,10 @@ lend2 amount balance = if amount < reserve * 0.5
     where reserve    = 100
           newBalance = balance - amount
 
+lend3 amount balance
+  | amount <= 0            = Nothing
+  | amount > reserve * 0.5 = Nothing
+  | otherwise              = Just newBalance
+  where reserve    = 100
+        newBalance = balance - amount
+
