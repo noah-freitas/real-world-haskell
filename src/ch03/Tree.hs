2b20fd47923dc7db13e74aa8750898f762c28994
Noah Freitas <noah.j.freitas@gmail.com>
2013-07-04 03:07:07 +0000

    ch03 Exercise 2

--- src/ch03/Tree.hs
+++ src/ch03/Tree.hs
@@ -1,8 +1,7 @@
 -- file: ch03/Tree.hs
-data Tree a = Node a (Tree a) (Tree a)
-            | Empty
+data Tree a = Node a (Maybe (Tree a)) (Maybe (Tree a))
               deriving (Show)
 
-simpleTree = Node "parent" (Node "left child" Empty Empty)
-                           (Node "right child" Empty Empty)
+simpleTree = Node "parent" (Just (Node "left child" Nothing Nothing))
+                           (Just (Node "right child" Nothing Nothing))
 
