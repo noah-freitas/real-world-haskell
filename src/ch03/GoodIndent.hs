eb812ff828b1566c03c76875ab61eaf331a73377
Noah Freitas <noah.j.freitas@gmail.com>
2013-07-04 17:01:34 +0000

    ch03/GoodIndent

---
+++ src/ch03/GoodIndent.hs
@@ -0,0 +1,9 @@
+-- file: ch03/GoodIndent.hs
+-- This is the left most column.
+
+  -- It's fine for top level declarations to start in any column ...
+  firstGoodIndentation = 1
+
+  -- ... provided all subsequent declarations do so too!
+  secondGoodIndentation = 2
+
